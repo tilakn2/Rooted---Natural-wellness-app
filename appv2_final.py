23a2f1774e94f51ab82eff8080b4d9a3a34c5d6b
Tilak Narendran <tilak.naren1@gmail.com>
2026-01-17 17:53:01 -0500

    Update appv2_final.py

--- appv2_final.py
+++ appv2_final.py
@@ -336,7 +336,13 @@ st.markdown('<h3 style="text-align: center;">Speak or Type Your Issue</h3>', uns
 #         tmp_filename = tmp_file.name
 col_left, col_center, col_right = st.columns([1, 2, 1])
 with col_center:
-    audio_bytes = audio_recorder(text="Click to record", icon_size="2x")
+    # audio_bytes = audio_recorder(text="Click to record", icon_size="2x")
+    audio_bytes = audio_recorder(
+    text="🎙️ Click to Record",
+    recording_color="#ff3300",
+    neutral_color="#5a7c3e",
+    icon_size="1x"
+)
 
 transcribed_text = ""
 
